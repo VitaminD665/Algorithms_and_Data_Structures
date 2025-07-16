d59640da0b6ea7bf2c708bde52d8fec75dc100c8
VitaminD665 <136028537+VitaminD665@users.noreply.github.com>
2025-07-15 21:55:30 -0400

    Second thirty minutes

---
+++ Second_30_mins.py
@@ -0,0 +1,14 @@
+"""
+July 15th, 2025, John Samis
+
+For time & space complexity and Big-O notation, always consider the worst case scenario.
+
+Binary Search: O(log(n))
+Linear Search: O(n)
+Quadratic Runtime: O(n^2)
+Cubic Runtime: O(n^3)
+Quasilinear: O(nlog(n))
+Exponential: O(x^n)
+
+Contained various analogies for explaining algorithm runtimes and whatnot.
+"""
